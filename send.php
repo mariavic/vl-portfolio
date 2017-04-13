b0e2ab1dcd1b28f2efbe80988acf0b5a19e5540a
Vicky Lopez <vickylopez@Vickys-MacBook-Pro-2.local>
2017-04-13 18:04:26 -0300

    favicons

---
+++ send.php
@@ -0,0 +1,22 @@
+<?php
+
+  $nombre = $_POST['name'];
+  $mail = $_POST['email'];
+  $asuntoUsuario = $_POST['subject'];
+  $mensaje = $_POST['message'];
+
+  $cuerpo = 'Remitente: ' . $nombre . '<br>';
+  $cuerpo .= 'Email: ' . $mail . '<br>';
+  $cuerpo .= 'Mensaje: ' . $message . '<br>';
+
+  $destino = 'vicky.8525@gmail.com';
+  $asunto = $asuntoUsuario;
+
+  $encabezados = 'MIME-Version: 1.0' . "\r\n";
+  $encabezados .= 'Content-type:text/html;charset=UTF-8' . "\r\n";
+  $encabezados .= 'From: ' . $nombre . '<' . $mail . '>' . "\r\n"; 
+
+  mail( $destino, $asunto, $cuerpo, $encabezados );
+  header("Location: thankyou.html"); 
+
+?>
